321ee15d82422d98b5dc227689b9b932f70bf49f
yellow-seed <42381113+yellow-seed@users.noreply.github.com>
2023-09-30 14:45:09 +0000

    Bluesky対応

--- hackathon_1/hackathon_1/main.py
+++ hackathon_1/hackathon_1/main.py
@@ -1,27 +1,38 @@
 from dotenv import load_dotenv
+from atproto import Client as bsclient
 import os
 import tweepy
 import json
 import boto3
 
-def main_func():
-    load_dotenv()
-    api_key = os.getenv('API_KEY')
-    api_secret = os.getenv('API_SECRET')
-    access_token = os.getenv('ACCESS_TOKEN')
-    access_secret = os.getenv('ACCESS_SECRET')
-    XAuth = tweepy.Client(consumer_key=api_key, consumer_secret=api_secret, access_token=access_token, access_token_secret=access_secret)
-    XAuth.create_tweet(text="tweepytest")
+def main_func(tweet):
+    post_x(tweet)
+    post_bluesky(tweet)
     
     return {
       'statusCode': 200,
       'body': json.dumps('Hello from Lambda!')
     }
+    
+def post_x(message):
+    api_key = os.getenv('API_KEY')
+    api_secret = os.getenv('API_SECRET')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_secret = os.getenv('ACCESS_SECRET')
+    XAuth = tweepy.Client(consumer_key=api_key, consumer_secret=api_secret, access_token=access_token, access_token_secret=access_secret)
+    XAuth.create_tweet(text=message)
+
+def post_bluesky(message):
+    api = bsclient()
+    api.login(os.getenv('BLUESKY_ID'), os.getenv('BLUESKY_PASSWORD'))
+    api.send_post(message)
 
 ################################
 # MAIN # デプロイ時にはlambda handlerの方のコメントを外してif __name__ == "__main__":をコメントアウトする
 ################################
-#def lambda_handler(event, context):
-if __name__ == "__main__":
+def lambda_handler(event, context):
+# if __name__ == "__main__":
+    # TODO: メッセージを受け取れるようにする
+    tweet = 'Hello from Lambda!'
     load_dotenv()
-    main_func()
+    main_func(tweet)
